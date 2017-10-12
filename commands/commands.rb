9c6471ad2ae0d22b458ecae42e4eed32792e3abe
mericda <mericda@gmail.com>
2017-10-11 23:55:20 -0400

    r

--- commands/commands.rb
+++ commands/commands.rb
@@ -19,14 +19,14 @@ module Commands
   REVERSE_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.freeze
 
 
-  def get_gif_for(query)
+  def get_gif_for query
 
 
   Giphy::Configuration.configure do |config|
     config.api_key = ENV["GIPHY_API_KEY"]
   end
 
-  results = Giphy.search(query, {limit: 50})
+  results = Giphy.search( query, {limit: 50})
   gif = nil
 
   #puts results.to_yaml
