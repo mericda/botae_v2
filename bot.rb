e284aa569dfa4831e42fec9072af3064e0b73b5d
mericda <mericda@gmail.com>
2017-10-12 04:18:16 -0400

    ree

--- bot.rb
+++ bot.rb
@@ -76,36 +76,7 @@ Bot.on :message do |message|
         say 'I could not get your name, sorry :('
       end
     end
-    if message_contains_location?
-    else
-      entities = @message.nlp["entities"]
-      puts "#{entities}"
-      keys = entities.keys
-      # store the entity with the
-      # highest confidence
-      entity_max = nil
-      confidence_max = 0
-      puts "#{keys.to_s}"
-      # iterate over the keys and find
-      #the one with the highest confidence
-      keys.each do |key|
-        confidence = entities[key].first['confidence']
-        confidence = confidence.to_f
-        puts "#{key} #{confidence}"
-        if confidence > confidence_max
-          entity_max = key
-          confidence_max = confidence
-        end
 
-      end
-      puts "Entity with max confidence: #{entity_max} #{confidence_max}"
-      if entity_max == 'greetings' && confidence_max > 0.9
-        say "Hello!"
-      elsif  entity_max == 'bye' && confidence_max > 0.9
-        say "bye!"
-
-      end
-    end
 
 
     # Use with block if you want to provide response behaviour
@@ -151,6 +122,36 @@ Bot.on :message do |message|
       #        if greetings && greetings.confidence > 0.8
       #          say 'it works',
       #      end
+    end
+    if message_contains_location?
+    else
+      entities = @message.nlp["entities"]
+      puts "#{entities}"
+      keys = entities.keys
+      # store the entity with the
+      # highest confidence
+      entity_max = nil
+      confidence_max = 0
+      puts "#{keys.to_s}"
+      # iterate over the keys and find
+      #the one with the highest confidence
+      keys.each do |key|
+        confidence = entities[key].first['confidence']
+        confidence = confidence.to_f
+        puts "#{key} #{confidence}"
+        if confidence > confidence_max
+          entity_max = key
+          confidence_max = confidence
+        end
+
+      end
+      puts "Entity with max confidence: #{entity_max} #{confidence_max}"
+      if entity_max == 'greetings' && confidence_max > 0.9
+        say "Hello!"
+      elsif  entity_max == 'bye' && confidence_max > 0.9
+        say "bye!"
+
+      end
     end
   end
 end
