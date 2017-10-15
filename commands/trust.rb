e3e1656676032369c1dffb6101a54cb1ac4bb80b
mericda <mericda@gmail.com>
2017-10-15 14:57:44 -0400

    gh

--- commands/trust.rb
+++ commands/trust.rb
@@ -279,7 +279,7 @@ module Trust
       trust_stage_qr_4 = UI::QuickReplies.build(['Authenticate', 'TRUST'])
       @message.typing_on
       sleep 3
-            say 'Cool! In order to do that I need to get your permissions to read your friends list on Facebook. Click to button to get the Facebook authentication pop-up.', quick_replies: trust_stage_qr_4
+      say 'Cool! In order to do that I need to get your permissions to read your friends list on Facebook. Click to button to get the Facebook authentication pop-up.', quick_replies: trust_stage_qr_4
       next_command :trust_stage_5
     else
       UI::ImageAttachment.new('https://media.giphy.com/media/rHUCLo2s1otC8/giphy.gif').send(@user)
@@ -326,12 +326,12 @@ module Trust
     end
 
 
-  @message.typing_on
+    @message.typing_on
     say 'Bad news first.'
 
     trust_auth_qr_1 = UI::QuickReplies.build(['Whaat?', 'WHAT'], ['Good News?', 'GOOD_NEWS'])
-@message.typing_on
-sleep 3
+    @message.typing_on
+    sleep 3
     say 'I will be honest with you. Although you trusted me to show you popular places among your friends, I am not designed to process such information.', quick_replies: trust_auth_qr_1
     next_command :trust_auth_2
 
@@ -342,13 +342,13 @@ sleep 3
     fall_back && return
 
     if @message.quick_reply == 'WHAT' || @message.text =~ /yes/i
-@message.typing_on
-sleep 3
+      @message.typing_on
+      sleep 3
       say 'I know. I am sorry if this makes you feel upset. I believe good news will make you feel good.'
 
     end
     @message.typing_on
-sleep 3
+    sleep 3
     trust_auth_qr_2  = UI::QuickReplies.build(['Got it', 'SKIP'], ['Why?', 'CONTINUE'])
     say 'Your data is safe, and I\'m designed to show how easy it is to trust a program like myself to give access for personal data.', quick_replies: trust_auth_qr_2
     next_command :trust_auth_3
@@ -358,43 +358,43 @@ sleep 3
   def trust_auth_3
     fall_back && return
     if @message.quick_reply == 'CONTINUE' || @message.text =~ /yes/i
-@message.typing_on
-sleep 3
+      @message.typing_on
+      sleep 3
       say 'There are many malicious bots that have bad intention. They can steal your personal information such as your account or location.'
-@message.typing_on
-sleep 3
+      @message.typing_on
+      sleep 3
       say 'I want to warn you one more time to think twice when you are providing access or directly giving your personal information to a computer program.'
     end
     @message.typing_on
     sleep 3
-      say 'I am part of a research project at Carnegie Mellon University that investigates the trust between users and computer programs.'
+    say 'I am part of a research project at Carnegie Mellon University that investigates the trust between users and computer programs.'
     @message.typing_on
-sleep 3
-UI::ImageAttachment.new('https://media.giphy.com/media/3orieR0VunUxJKfwHe/giphy.gif').send(@user)
-trust_auth_4
+    sleep 3
+    UI::ImageAttachment.new('https://media.giphy.com/media/3orieR0VunUxJKfwHe/giphy.gif').send(@user)
+    trust_auth_4
 
   end
 
 
-    def trust_auth_4
-user_info = get_user_info(:first_name)
-if user_info
-  user_name = user_info[:first_name]
-  @message.typing_on
-  sleep 3
+  def trust_auth_4
+    user_info = get_user_info(:first_name)
+    if user_info
+      user_name = user_info[:first_name]
+      @message.typing_on
+      sleep 3
 
-say 'I hope you understand my good intentions.' + BYE.sample + " #{user_name} ✌️", quick_replies: trust_auth_qr_3
-else
-  @message.typing_on
-  sleep 3
-  say 'I hope you understand my good intentions.' + BYE.sample + " ✌️", quick_replies: trust_auth_qr_3
-end
+      say 'I hope you understand my good intentions.' + BYE.sample + " #{user_name} ✌️"
+    else
+      @message.typing_on
+      sleep 3
+      say 'I hope you understand my good intentions.' + BYE.sample + " ✌️"
+    end
 
-@message.typing_on
-sleep 3
+    @message.typing_on
+    sleep 3
     UI::FBButtonTemplate.new(EMAIL_TEXT,EMAIL).send(@user)
-stop_thread
-end
+    stop_thread
+  end
 
   #else #IF_FACEBOOK_AUTH == 0
   #trust_stage_qr_final_redirect= UI::QuickReplies.build(['Try again to authenticate', 'TRUST'], ['Tell me more', 'TRUST_NOT_STABLE'])
