9b4b2d3afe65542c0a60284030013af5865e0fe0
mericda <mericda@gmail.com>
2017-10-15 02:00:32 -0400

    assad

--- commands/trust.rb
+++ commands/trust.rb
@@ -11,6 +11,103 @@ EMAIL = [
     title: "Visit project page"
   }
 ].freeze
+
+
+COFFEE = [
+  {
+    title: 'Random image',
+    # Horizontal image should have 1.91:1 ratio
+    image_url: 'https://unsplash.it/760/400?random',
+    subtitle: "That's a first card in a carousel",
+    default_action: {
+      type: 'web_url',
+      url: 'https://unsplash.it'
+    },
+    buttons: [
+      {
+        type: :web_url,
+        url: 'https://unsplash.it',
+        title: 'Website'
+      },
+      {
+        type: :postback,
+        title: 'Square Images',
+        payload: 'SQUARE_IMAGES'
+      }
+    ]
+  },
+  {
+    title: 'Another random image',
+    # Horizontal image should have 1.91:1 ratio
+    image_url: 'https://unsplash.it/600/315?random',
+    subtitle: "And here's a second card. You can add up to 10!",
+    default_action: {
+      type: 'web_url',
+      url: 'https://unsplash.it'
+    },
+    buttons: [
+      {
+        type: :web_url,
+        url: 'https://unsplash.it',
+        title: 'Website'
+      },
+      {
+        type: :postback,
+        title: 'Unsquare Images',
+        payload: 'HORIZONTAL_IMAGES'
+      }
+    ]
+  }
+].freeze
+
+
+FOOD = [
+  {
+    title: 'Random image',
+    # Horizontal image should have 1.91:1 ratio
+    image_url: 'https://unsplash.it/760/400?random',
+    subtitle: "That's a first card in a carousel",
+    default_action: {
+      type: 'web_url',
+      url: 'https://unsplash.it'
+    },
+    buttons: [
+      {
+        type: :web_url,
+        url: 'https://unsplash.it',
+        title: 'Website'
+      },
+      {
+        type: :postback,
+        title: 'Square Images',
+        payload: 'SQUARE_IMAGES'
+      }
+    ]
+  },
+  {
+    title: 'Another random image',
+    # Horizontal image should have 1.91:1 ratio
+    image_url: 'https://unsplash.it/600/315?random',
+    subtitle: "And here's a second card. You can add up to 10!",
+    default_action: {
+      type: 'web_url',
+      url: 'https://unsplash.it'
+    },
+    buttons: [
+      {
+        type: :web_url,
+        url: 'https://unsplash.it',
+        title: 'Website'
+      },
+      {
+        type: :postback,
+        title: 'Unsquare Images',
+        payload: 'HORIZONTAL_IMAGES'
+      }
+    ]
+  }
+].freeze
+
 module Trust
   # State 'module_function' before any method definitions so
   # commands are mixed into Dispatch classes as private methods.
@@ -24,8 +121,11 @@ module Trust
 
     if @message.quick_reply == 'TRUST_STAGE_1_CHOICE_A' || @message.text =~ /yes/i
       say 'Nice! Let me see if I can find ☕️ better than Starbucks.'
+      choice = 'coffee'
+
     else
       say 'Nice! Let me see if I can find 🍽 better than Subway.'
+        choice = 'food'
     end
     #log = @message.text
     say 'Send me your location by clicking the button below and I \'ll tell you what\'s the location close to you.', quick_replies: LOCATION_PROMPT
@@ -52,8 +152,11 @@ module Trust
     address = extract_full_address(parsed)
     say "Looks like you're at #{address}"
     @message.typing_off
-
-    show_carousel(image_ratio: :square)
+if choice == 'coffee'
+    UI::FBCarousel.new(COFFEE).send(@user)
+  elsif choice == 'food'
+    UI::FBCarousel.new(FOOD).send(@user)
+end
     trust_stage_qr_3_1 = UI::QuickReplies.build(['Yes', 'TRUST_STABLE'], ['No', 'TRUST_NOT_STABLE'])
     say 'Did you like it?', quick_replies: trust_stage_qr_3_1
     next_command :trust_stage_3
@@ -87,11 +190,6 @@ entity_check
       trust_stage_qr_feedback= UI::QuickReplies.build(*NAY_FEEDBACK)
       say 'Let me know why you didn\'t like it. ', quick_replies: trust_stage_qr_feedback
     end
-  next_command :trust_stage_3_2
-  end
-
-  def trust_stage_3_2
-
     trust_stage_qr_3_2 = UI::QuickReplies.build(['Yes', 'TRUST_CONFIRMATION_INTENT'], ['No', 'TRUST_NOT_STABLE'])
     say 'Alright, are you ready to see the most popular places among your Facebook friends?', quick_replies: trust_stage_qr_3_2
     next_command :trust_stage_4
@@ -137,10 +235,12 @@ entity_check
     # if FACEBOOK_AUTH == 1
     fall_back && return
 
-    say 'Now, give me some time while I am looking what your friends did.'
-    say 'I will let you know when I am ready to share the results.'
-        sleep 120
-    say 'user_name, I have both good and bad news.'
+  #  say 'Now, give me some time while I am looking what your friends did.'
+  #  say 'I will let you know when I am ready to share the results.'
+    user_info = get_user_info(:first_name)
+    if user_info
+      user_name = user_info[:first_name]
+    say "#{user_name}, I have both good and bad news."
 
 
     say 'Bad news first.'
@@ -153,36 +253,22 @@ entity_check
       say 'I know. I am sorry if this makes you feel upset. I believe good news will make you feel good.'
     end
 
-    UI::ImageAttachment.new('https://media.giphy.com/media/3orieR0VunUxJKfwHe/giphy.gif').send(@user)
-
     trust_auth_qr_1  = UI::QuickReplies.build(['Got it', 'SKIP'], ['Tell me more', 'CONTINUE'])
     say 'Your data is safe, and I\'m designed to show how easy it is to trust a program like myself to give access for personal data.', quick_replies: trust_auth_qr_2
 
     if @message.quick_reply == 'CONTINUE' || @message.text =~ /yes/i
-      say 'There are many malicious bots that have bad intentions such as stealing personal information such as your accounnt or location information.'
+      say 'There are many malicious bots that have bad intentions such as stealing personal information such as your account or location information.'
+      say 'I want to warn you one more time to think twice when you are providing access or directly giving your personal information to a computer program.'
     end
 
 
-
-
-
-
-    @message.typing_on
-    say 'I am part of a research project at Carnegie Mellon University School of Design that investigates the trust between users and computer programs,'
-    @message.typing_off
-    @message.typing_on
-    say 'I want to warn you one more time to think twice when you are providing access or directly giving your personal information to a computer program.'
-    @message.typing_off
-
-
-    @message.typing_on
+    say 'I am part of a research project at Carnegie Mellon University School of Design that investigates the trust between users and computer programs.'
     say 'I hope you understand my good intentions. '
-    @message.typing_off
-    @message.typing_on
     say 'If you have questions or comments about this research, please e-mail the researcher, Meric Dagli from mericda@cmu.edu or visit the project page below.'
-@message.typing_off
     UI::FBButtonTemplate.new(EMAIL_TEXT,EMAIL).send(@user)
     say 'Thank you! '
+    UI::ImageAttachment.new('https://media.giphy.com/media/3orieR0VunUxJKfwHe/giphy.gif').send(@user)
+
     stop_thread
     #else #IF_FACEBOOK_AUTH == 0
     #trust_stage_qr_final_redirect= UI::QuickReplies.build(['Try again to authenticate', 'TRUST'], ['Tell me more', 'TRUST_NOT_STABLE'])
