fd87836c3e153cdb3dc16fd6f01d58fcf745cc44
mericda <mericda@gmail.com>
2017-10-16 15:53:10 -0400

    migration text

---
+++ db/migrate/20171016194206_a_descriptive_name_for_my_migration.rb
@@ -0,0 +1,4 @@
+class ADescriptiveNameForMyMigration < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
---
+++ db/migrate/20171016194232_create_logs_table.rb
@@ -0,0 +1,15 @@
+class CreateLogsTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :logs do |t|
+
+      t.integer :message_id
+      t.integer :user_id
+      t.integer :flow_id
+      t.integer :intent_id
+      t.text :raw_message
+      boolean :has_attachment
+      t.timestamps
+
+    end
+  end
+end
